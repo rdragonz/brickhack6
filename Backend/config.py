182361062f6140d08ff7686f97fa9ad4ebcb4e22
Andrew Ziogas <crayman122@gmail.com>
2020-02-08 16:22:57 -0500

    Added configuration option used for database configuration

---
+++ Backend/config.py
@@ -0,0 +1,15 @@
+'''
+Configuration settings for the server. Read on server load.
+'''
+
+#The address for the database server used
+DB_ADDRESS = "localhost"
+
+#The port that the database connects with
+DB_PORT = "27017"
+
+#The user to connect to the database as
+DB_USER = "<username>"
+
+#The password of the above listed user to connect to the database
+DP_PASS = "<password>"
