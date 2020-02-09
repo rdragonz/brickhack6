a766fa90d4638d1bdfff960af4331f13451dc4b5
Kawagga Kyewalyanga Naggenda <nagzie@Nagzies-MacBook-Air.local>
2020-02-09 01:16:33 -0500

    SideBar

--- Frontend/webapp/src/App.js
+++ Frontend/webapp/src/App.js
@@ -1,33 +1,23 @@
 import React from 'react';
 import './App.css';
 import SearchBar from './Components/SearchBar/SearchBar';
-import SearchBar2 from './Components/SearchBar/SearchBar2';
-import ContentPane from './Components/ContentPane/ContentPane';
 
 
 
-<<<<<<< HEAD
-function App() {
-=======
-class App extends React.Component{
+class App extends React.Component {
+  render (){
+    return (
+      <div className="App">
+        <SearchBar/>
+      
+        <header className="App-header">
+          Our Website 
+        </header>
+      </div>
+    );
 
-  onSearchSubmit(input) {
-    console.log(input);
   }
-
- render (){ 
->>>>>>> 7591b901d005266d39c447445da7e3e90b86bdb0
-  return (
-    <div className="App">
-      {/*<SearchBar onSubmit={this.onSearchSubmit} /> */}
-      <SearchBar2 onSubmit={this.onSearchSubmit}/>
-      <ContentPane />
-      <header className="App-header">
-    
-      </header>
-    </div>
-  );
-  }
+  
 }
 
 export default App;
