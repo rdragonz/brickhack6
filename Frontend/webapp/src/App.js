2dba6755002dfe5025495c9dd3f8ef7e1d8991f5
Michael Sollazzo <msollazzo08@gmail.com>
2020-02-08 20:02:21 -0500

    new search bar

--- Frontend/webapp/src/App.js
+++ Frontend/webapp/src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import './App.css';
 import SearchBar from './Components/SearchBar/SearchBar';
+import SearchBar2 from './Components/SearchBar/SearchBar2';
 
 
 
@@ -13,7 +14,8 @@ class App extends React.Component{
  render (){ 
   return (
     <div className="App">
-      <SearchBar onSubmit={this.onSearchSubmit} />
+      {/*<SearchBar onSubmit={this.onSearchSubmit} /> */}
+      <SearchBar2 onSubmit={this.onSearchSubmit}/>
       <header className="App-header">
         Our Website 
       </header>
---
+++ Frontend/webapp/src/Components/SearchBar/SearchBar2.js
@@ -0,0 +1,47 @@
+import React from 'react';
+import './SearchBar.css';
+
+class SearchBar2 extends React.Component {
+  
+  state = {input: ''};
+  
+  onInputChange(event) {
+    console.log(event.target.value);
+  }
+
+  onInputClick(){
+    console.log('Input was clicked');
+  }
+
+  onFormSubmit = (event) => {
+    event.preventDefault();
+    this.props.onSubmit(this.state.input);
+  };
+
+
+  render (){
+    return (
+      <div className='ui segment'>
+        <form  className='ui form'>
+          <div class="ui search">
+           <div class="ui icon input">
+            <input
+              class="prompt" 
+              type="text"
+              placeholder="Seach Item..."
+              value={this.state.input}
+              onChange={(e) => this.setState({input: e.target.value })} 
+              />
+            <i class="search icon"></i>
+          </div>
+          <div class="results"></div>
+        </div>
+      </form>
+    </div>
+     
+    )
+  
+  }
+};
+
+export default SearchBar2;
