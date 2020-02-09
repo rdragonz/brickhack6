4dad4728f7232cce6d68bb3e9845e13ecab5bfad
Red Ziogas <ajz1369@rit.edu>
2020-02-09 00:49:57 -0500

    Create post management module

---
+++ Backend/posts/__init__.py
@@ -0,0 +1,26 @@
+import random
+import datetime
+
+def newPostID(database):
+	'''
+	Generate a new post ID.
+	Needs a database object to verify that the ID doesn't already exist
+	'''
+	possibleID = random.randint(0, 4294967295) #Generate a new ID between 0 and the 32 bit integer limit
+
+	if database["posts"].find_one({"postid":str(possibleID)}) == None: #Check if the ID already exists
+		return possibleID
+	else:
+		#In the absolutely amazingly unlikely event that the generated ID already exists, generate a new one
+		return newPostID(database)
+
+def newTextPost(database, userid, location, text, tags):
+	'''
+	Create a new post with text
+	'''
+	postID = newPostID(database) #Create a new valid ID
+
+	time = datetime.datetime.now(datetime.timezone.utc) #Get the current UTC time
+
+	database['posts'].insert_one({"postid" : str(postID),"userid" : userid, "type" : "text", "timestamp" : str(time), "location" : location, "content" : { "text" : text },
+    "tags" : tags})
