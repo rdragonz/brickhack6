a6dc06b7ad8a754a1436dfac47a420fd41ceb9d2
Kawagga Kyewalyanga Naggenda <knaggenda@gmail.com>
2020-02-09 01:56:47 -0500

    sick of commits mna

--- Frontend/webapp/src/App.js
+++ Frontend/webapp/src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './App.css';
-import SearchBar2 from './Components/SearchBar/SearchBar';
+import SearchBar2 from './Components/SearchBar/SearchBar2';
 import ContentPane from './Components/ContentPane/ContentPane';
 
 
