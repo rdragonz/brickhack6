efc60082b831c4ee6ada7a6de7bac49f3dbcbe8f
Red Ziogas <ajz1369@rit.edu>
2020-02-09 05:33:39 -0500

    Return data as json

--- Backend/queries/__init__.py
+++ Backend/queries/__init__.py
@@ -4,8 +4,10 @@ Code for simple queries
 
 
 import queries.distance
+from bson import json_util
 
-def postsInRadius(database, direction, distance, lat, lon):
+
+def postsInRadius(database, distance, lat, lon):
 	'''
 	Query posts based on location
 	'''
@@ -19,11 +21,11 @@ def postsInRadius(database, direction, distance, lat, lon):
 	#Find the correct query by finding the relationship between the values
 	if northPoint > southPoint:
 		if eastPoint > westPoint:
-			return database['posts'].find([{"$match":{"$and":[{"lat":{"$lt":northPoint}},{"lat":{"$gt":southPoint}},{"lon":{"$lt":westPoint}},{"lon":{"$gt":eastPoint}}]}}])
+			return json.dumps(database['posts'].find([{"$match":{"$and":[{"lat":{"$lt":northPoint}},{"lat":{"$gt":southPoint}},{"lon":{"$lt":westPoint}},{"lon":{"$gt":eastPoint}}]}}]))
 		else:
-			return database['posts'].find([{"$match":{"$and":[{"lat":{"$lt":northPoint}},{"lat":{"$gt":southPoint}},{"lon":{"$lt":eastPoint}},{"lon":{"$gt":westPoint}}]}}])
+			return json.dumps(database['posts'].find([{"$match":{"$and":[{"lat":{"$lt":northPoint}},{"lat":{"$gt":southPoint}},{"lon":{"$lt":eastPoint}},{"lon":{"$gt":westPoint}}]}}]))
 	else:
 		if eastPoint > westPoint:
-			return database['posts'].find([{"$match":{"$and":[{"lat":{"$lt":southPoint}},{"lat":{"$gt":northPoint}},{"lon":{"$lt":westPoint}},{"lon":{"$gt":eastPoint}}]}}])
+			return json.dumps(database['posts'].find([{"$match":{"$and":[{"lat":{"$lt":southPoint}},{"lat":{"$gt":northPoint}},{"lon":{"$lt":westPoint}},{"lon":{"$gt":eastPoint}}]}}]))
 		else:
-			return database['posts'].find([{"$match":{"$and":[{"lat":{"$lt":southPoint}},{"lat":{"$gt":northPoint}},{"lon":{"$lt":eastPoint}},{"lon":{"$gt":westPoint}}]}}])
+			return json.dumps(database['posts'].find([{"$match":{"$and":[{"lat":{"$lt":southPoint}},{"lat":{"$gt":northPoint}},{"lon":{"$lt":eastPoint}},{"lon":{"$gt":westPoint}}]}}]))
